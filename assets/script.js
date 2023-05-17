077ddaa05a4f6b1a856f7c274503addbcd2f63d7
irene <panisirene@gmail.com>
2023-05-17 02:47:07 -0700

    Add save teams feature using local storage

--- assets/script.js
+++ assets/script.js
@@ -20,8 +20,6 @@ fetch(pokeAPIcards)
     return response.json();
   })
   .then(function (data) {
-  
-
     for (let i = 0; i < 1008; i++) {
       var pokemonName = data.results[i].name;
       pokemonList.push(pokemonName);
@@ -40,16 +38,10 @@ fetch(pokeAPIcards)
       $(pokeCard).append(addTeamBtn);
       $(pokeCard).attr('id', pokemonName);
       $(pokeCards).append(pokeCard);
-      // assigning datasets onto the pokeCard div tag. 
-      $(pokeCard).data('name', pokemonName);
-      $(pokeCard).data('num', pokeImageNumber);
-
-
     }
+  });
 
-  }
-  );
-
+// searchbar autocomplete with limited results (max 10 at a time)
 $("#PokeName").autocomplete({
   source: function (request, response) {
     var results = $.ui.autocomplete.filter(pokemonList, request.term);
@@ -59,11 +51,9 @@ $("#PokeName").autocomplete({
 
 //event Listener when the click button on the poke card is clicked 
 $(document).on('click', '.clickModalBtn', function () {
-  // the url to get pokemon stat https://pokeapi.co/api/v2/pokemon/{id or name}/
-  var parentEl = $(this).parent()
-  var parentDataName = parentEl.data('name')
-  var parentDataNum = parentEl.data('num')
-  var pokedexUrl = 'https://pokeapi.co/api/v2/pokemon/' + parentDataName + '/'
+  // grabs name of pokemon we're looking at so we can use it to fetch pokemon info
+  var parentEl = $(this).siblings( 'p' ).text();
+  var pokedexUrl = 'https://pokeapi.co/api/v2/pokemon/' + parentEl + '/'
   fetch(pokedexUrl).then(
     function (response) {
       if (response.ok) {
@@ -76,31 +66,28 @@ $(document).on('click', '.clickModalBtn', function () {
             chosenPokemonType.toString()
           }
 
-          $(modalPokemonName).text(parentDataName)
-          $(modalImg).attr('src', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/' + parentDataNum + '.png')
+          // retrieves pokemon number and formats it to work with img url
+          var pokemonNum = data.id.toString().padStart(3, '0');
+          $(modalPokemonName).text(parentEl)
+          $(modalImg).attr('src', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/' + pokemonNum + '.png')
           $(pokeHeight).text('Height: ' + chosenPokemonHeight)
           $(pokeWeight).text('Weight: ' + chosenPokemonWeight)
           $(pokeType).text('Type(s): ' + chosenPokemonType.toString())
-
-
-
-
         })
       }
     }
   )
   $(modal).css("display", "block");
-
-})
+});
 
 
 // clear all info in the modalInfoDiv tag
 function clearPreviousModalInfoDiv() {
-  $(modalPokemonName).empty()
-  $(modalImg).attr('src', '')
-  $(pokeHeight).empty()
-  $(pokeWeight).empty()
-  $(pokeType).empty()
+  $(modalPokemonName).empty();
+  $(modalImg).attr('src', '');
+  $(pokeHeight).empty();
+  $(pokeWeight).empty();
+  $(pokeType).empty();
 }
 
 
@@ -113,7 +100,7 @@ $(spanClose).click(function () {
 // scrolls down to searched pokemon and handles invalid searches
 $('#search-btn').on('click', function () {
   var name = $('#PokeName').val();
-  if (pokemonList.includes(name)) {
+  if (pokemonList.includes(name)) { // checks search validity
     $("#PokeName").attr("style", "border:");
     $("#searchform").removeClass('invalid');
     var pokeId = "#" + $('#PokeName').val();
@@ -125,7 +112,7 @@ $('#search-btn').on('click', function () {
     setTimeout(function () {
       $(pokeId).attr("style", "background-color:; transition: 0.5s");
     }, 2000);
-  } else {
+  } else { // if invalid search we style searchbar and tell user it's invalid
     $("#PokeName").attr("style", "border: 2px solid red");
     $("#searchform").addClass('invalid');
   }
@@ -133,24 +120,18 @@ $('#search-btn').on('click', function () {
 
 // event listener to create a new team of pokemon
 $(document).on('click', '.addTeamBtn', function () {
-  var teamParentEl = $(this).parent()
-  var teamDataName = teamParentEl.data('name')
-  var teamDataNum = teamParentEl.data('num')
-  //var teamImg = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/' + teamDataNum + '.png'
-  // var generatedTeamDiv = $('<div class= "generatedTeam ">');
+  var teamParentEl = $(this).parent();
 
-  console.log($('.generatedTeam').children().length);
-
+  // handles team length being more than 6
   if ($('.generatedTeam').children().length < 6) {
     var teamSlotDiv = $('<div class="slot">');
     var removePokeCardBtn = $('<button id="removeBtn" class="rounded-full bg-red-300 " type="button">Remove</button>')
-    //var teamImgEl = $('<img>');
-    //$(teamImgEl).attr('src', teamImg);
+    
+    // creates clone of pokemon card we want to add to team
     var pokeCardsClone = $(teamParentEl).clone(true).removeAttr('id');
-    pokeCardsClone.find('.addTeamBtn').remove()
+    pokeCardsClone.find('.addTeamBtn').remove();
 
-    $(pokeCardsClone).append(removePokeCardBtn)
-    console.log(pokeCardsClone);
+    $(pokeCardsClone).append(removePokeCardBtn);
 
     $(pokeCardsClone).appendTo(teamSlotDiv);
     $('.generatedTeam').append(teamSlotDiv);
@@ -158,42 +139,46 @@ $(document).on('click', '.addTeamBtn', function () {
   }
 });
 
+// create team button
 $(document).on('click', '#creatNewTeamDiv', function () {
   var generatedTeamDiv = $('<div class= "generatedTeam flex ">');
-  $(teamDiv).append(generatedTeamDiv)
- $(this).css('display', 'none')
+  $(teamDiv).append(generatedTeamDiv);
+  $(this).css('display', 'none');
 })
 
-
+// save team button
 $(document).on('click', '#saveToTeam', function () {
   var slots = $(".generatedTeam").children();
+  savedPokemon = []; // empties savedPokemon array to clear data for each save
   for (let i=0; i < slots.length; i++) {
-    console.log(slots[i])
-    $(savedPokemon).push($(slots[i]));
+    savedPokemon.push(slots[i].outerHTML); // saves html content of each child (slot/card) so we can store and display later
   }
-  localStorage.setItem("team", savedPokemon.innerHTML);
-}
+  localStorage.setItem("team", JSON.stringify(savedPokemon));
+});
 
-)
- 
+// initialize, retrieve local storage
 function init() {
   var team = localStorage.getItem("team");
-  console.log(team)
-  if (team === null) {
-    savedPokemon= [];
-  }
-  else {
-    savedPokemon= JSON.parse(team);
+  // handles if local storage is empty or team is empty
+  if (team === null || team === "[]") {
+    savedPokemon = [];
+  } else {
+    // if we have an existing team we grab it, make a container for it
+    // then display each card one by one
+    savedPokemon = JSON.parse(team);
+    var generatedTeamDiv = $('<div class= "generatedTeam flex ">');
+    $(teamDiv).append(generatedTeamDiv);
+    for (let i = 0; i < savedPokemon.length; i++) {
+      generatedTeamDiv.append($(savedPokemon[i]));
+    }
+    // hides create team button since we already have one
+    $( '#creatNewTeamDiv ').css('display', 'none');
   }
 }
 
-
-init();
-
-
-
 $(document).on('click', '#removeBtn', function () {
-  $(this).parent().parent().remove()
+  $(this).parent().parent().remove();
+});
 
-})
-
+// initialize
+init();
